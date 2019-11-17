8f779a1ff946b062547614fe89359d5a0798e07a
Hyounggyu Kim <code@hyounggyu.com>
2019-11-17 18:51:38 +0900

    Update static

--- cupweb/settings/base.py
+++ cupweb/settings/base.py
@@ -139,7 +139,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, 'static_compiled'),
+    os.path.join(BASE_DIR, 'static_compiled'),
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/2.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_compiled')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
