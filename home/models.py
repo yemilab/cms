f60003eb80bbe0d8d02b770f6f99777d997b9dfb
Hyounggyu Kim <code@hyounggyu.com>
2019-11-17 11:43:07 +0900

    Update

--- home/models.py
+++ home/models.py
@@ -1,6 +1,36 @@
 from django.db import models
 
+from modelcluster.models import ClusterableModel
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    MultiFieldPanel,
+)
 from wagtail.core.models import Page
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class People(ClusterableModel):
+    first_name = models.CharField("First name", max_length=254)
+    last_name = models.CharField("Last name", max_length=254)
+
+    panels = [
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('first_name', classname="col6"),
+                FieldPanel('last_name', classname="col6"),
+            ])
+        ], "Name"),
+    ]
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
 
 
 class HomePage(Page):
--- publication/models.py
+++ publication/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+class PresentationPeopleRelationship(Orderable, models.Model):
+    page = ParentalKey(
+        'PresentationPage', related_name='presentation_person_relationship', on_delete=models.CASCADE
+    )
+    people = models.ForeignKey(
+        'home.People', related_name='person_presentation_relationship', on_delete=models.CASCADE
+    )
+    panels = [
+        SnippetChooserPanel('people')
+    ]
+
+
+class PresentationPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'PresentationPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+class PresentationPage(Page):
+    date = models.DateField("Presentation data")
+    abstract = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=PresentationPageTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        InlinePanel(
+            'presentation_person_relationship', label="Presentor(s)",
+            panels=None, min_num=1),
+        FieldPanel('tags'),
+        FieldPanel('abstract', classname="full")
+    ]
