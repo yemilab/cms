b2533fc8b7b043b1ecb19e02072dd4107b667443
Hyounggyu Kim <code@hyounggyu.com>
2019-11-17 12:00:04 +0900

    Add meeting snippet

--- publication/models.py
+++ publication/models.py
@@ -7,6 +7,7 @@ from taggit.models import TaggedItemBase
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.snippets.models import register_snippet
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 
@@ -29,12 +30,37 @@ class PresentationPageTag(TaggedItemBase):
         on_delete=models.CASCADE
     )
 
+@register_snippet
+class Meeting(models.Model):
+    name = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    panels = [
+        FieldPanel('name'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Meetings"
+
+
 class PresentationPage(Page):
     date = models.DateField("Presentation data")
     abstract = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=PresentationPageTag, blank=True)
+    meeting = models.ForeignKey(
+        'publication.Meeting',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
 
     content_panels = Page.content_panels + [
+        FieldPanel('meeting'),
         FieldPanel('date'),
         InlinePanel(
             'presentation_person_relationship', label="Presentor(s)",
