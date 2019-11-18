eaa09509c772306db32b5033f25200ac384fb5cc
Hyounggyu Kim <code@hyounggyu.com>
2019-11-18 13:34:46 +0900

    Add papers

--- publication/models.py
+++ publication/models.py
@@ -7,9 +7,47 @@ from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+@register_snippet
+class Meeting(models.Model):
+    name = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Meetings"
+
+
+@register_snippet
+class Journal(models.Model):
+    title = models.CharField(max_length=250)
+    abbreviation = models.CharField(max_length=100, blank=True)
+    doi = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Journals"
+
+
+@register_snippet
+class Collaboration(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Collaborations"
 
 
 class PresentationPeopleRelationship(Orderable, models.Model):
@@ -31,19 +69,6 @@ class PresentationPageTag(TaggedItemBase):
         on_delete=models.CASCADE
     )
 
-@register_snippet
-class Meeting(models.Model):
-    name = models.CharField(max_length=250)
-    city = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "Meetings"
-
 
 class PresentationPage(Page):
     date = models.DateField("Presentation date")
@@ -51,6 +76,12 @@ class PresentationPage(Page):
     tags = ClusterTaggableManager(through=PresentationPageTag, blank=True)
     meeting = models.ForeignKey(
         'publication.Meeting',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    collaboration = models.ForeignKey(
+        'publication.Collaboration',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
@@ -83,8 +114,7 @@ class PresentationsIndexPage(Page):
     parent_page_types = ['PublicationsIndexPage', ]
 
     def get_presentations(self):
-        return PresentationPage.objects.live().descendant_of(
-            self).order_by('-date')
+        return PresentationPage.objects.live().descendant_of(self).order_by('-date')
 
     def children(self):
         return self.get_children().specific().live()
@@ -107,6 +137,104 @@ class PresentationsIndexPage(Page):
         return context
 
 
+class PaperPeopleRelationship(Orderable, models.Model):
+    page = ParentalKey(
+        'PaperPage', related_name='paper_person_relationship', on_delete=models.CASCADE
+    )
+    people = models.ForeignKey(
+        'home.People', related_name='person_paper_relationship', on_delete=models.CASCADE
+    )
+    panels = [
+        SnippetChooserPanel('people')
+    ]
+
+
+class PaperPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'PaperPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
+class PaperPage(Page):
+    authors = models.CharField(max_length=10240, blank=True)
+    collaboration = models.ForeignKey(
+        'publication.Collaboration',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    date = models.DateField("Presentation date")
+    journal = models.ForeignKey(
+        'publication.Journal',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    page = models.CharField(max_length=100, blank=True)
+    volume = models.CharField(max_length=100, blank=True)
+    issue = models.CharField(max_length=100, blank=True)
+    abstract = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=PresentationPageTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('authors'),
+        FieldPanel('collaboration'),
+        InlinePanel(
+            'paper_person_relationship', label="Corresponding author(s)",
+            panels=None, min_num=1),
+        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('journal'),
+            FieldPanel('page'),
+            FieldPanel('volume'),
+            FieldPanel('issue'),
+        ], heading="Journal section"),
+        FieldPanel('tags'),
+        FieldPanel('abstract', classname="full")
+    ]
+
+    subpage_types = [ ]
+    parent_page_types = ['PapersIndexPage', ]
+
+
+class PapersIndexPage(Page):
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+    ]
+
+    subpage_types = ['PaperPage', ]
+    parent_page_types = ['PublicationsIndexPage', ]
+
+    def get_papers(self):
+        return PaperPage.objects.live().descendant_of(self).order_by('-date')
+
+    def children(self):
+        return self.get_children().specific().live()
+
+    def paginate(self, request, *args):
+        page = request.GET.get('page')
+        paginator = Paginator(self.get_papers(), 12)
+        try:
+            pages = paginator.page(page)
+        except PageNotAnInteger:
+            pages = paginator.page(1)
+        except EmptyPage:
+            pages = paginator.page(paginator.num_pages)
+        return pages
+
+    def get_context(self, request):
+        context = super(PapersIndexPage, self).get_context(request)
+        papers = self.paginate(request, self.get_papers())
+        context['papers'] = papers
+        return context
+
+
 class PublicationsIndexPage(Page):
     introduction = models.TextField(
         help_text='Text to describe the page',
@@ -116,4 +244,4 @@ class PublicationsIndexPage(Page):
         FieldPanel('introduction', classname="full"),
     ]
 
-    subpage_types = ['PresentationsIndexPage', ]
+    subpage_types = ['PresentationsIndexPage', 'PapersIndexPage']
