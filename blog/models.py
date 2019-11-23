23e38bff99407d74651e760ec1b8b6da295f8417
Hyounggyu Kim <code@hyounggyu.com>
2019-11-23 20:17:02 +0900

    Add blog model
    Copy from bakerydemo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,147 @@
+from django.contrib import messages
 from django.db import models
+from django.shortcuts import redirect, render
 
-# Create your models here.
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+
+from taggit.models import Tag, TaggedItemBase
+
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, RichTextField
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page, Orderable
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+class BlogPeopleRelationship(Orderable, models.Model):
+    page = ParentalKey(
+        'BlogPage', related_name='blog_person_relationship', on_delete=models.CASCADE
+    )
+    people = models.ForeignKey(
+        'home.People', related_name='person_blog_relationship', on_delete=models.CASCADE
+    )
+    panels = [
+        SnippetChooserPanel('people')
+    ]
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPage', related_name='tagged_items', on_delete=models.CASCADE)
+
+
+class BlogPage(Page):
+    subtitle = models.CharField(blank=True, max_length=255)
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+    cover_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+    body = RichTextField()
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    date_published = models.DateField(
+        "Date article published", blank=True, null=True
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle', classname="full"),
+        FieldPanel('introduction', classname="full"),
+        ImageChooserPanel('cover_image'),
+        FieldPanel('body'),
+        FieldPanel('date_published'),
+        InlinePanel(
+            'blog_person_relationship', label="Author(s)",
+            panels=None, min_num=1),
+        FieldPanel('tags'),
+    ]
+
+    def authors(self):
+        return [ n.people for n in self.blog_person_relationship.all() ]
+
+    @property
+    def get_tags(self):
+        tags = self.tags.all()
+        for tag in tags:
+            tag.url = '/' + '/'.join(s.strip('/') for s in [
+                self.get_parent().url,
+                'tags',
+                tag.slug
+            ])
+        return tags
+
+    parent_page_types = ['BlogIndexPage']
+    subpage_types = []
+
+
+class BlogIndexPage(RoutablePageMixin, Page):
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+        ImageChooserPanel('image'),
+    ]
+
+    subpage_types = ['BlogPage']
+
+    def children(self):
+        return self.get_children().specific().live()
+
+    def get_context(self, request):
+        context = super(BlogIndexPage, self).get_context(request)
+        context['posts'] = BlogPage.objects.descendant_of(
+            self).live().order_by(
+            '-date_published')
+        return context
+
+    @route(r'^tags/$', name='tag_archive')
+    @route(r'^tags/([\w-]+)/$', name='tag_archive')
+    def tag_archive(self, request, tag=None):
+
+        try:
+            tag = Tag.objects.get(slug=tag)
+        except Tag.DoesNotExist:
+            if tag:
+                msg = 'There are no blog posts tagged with "{}"'.format(tag)
+                messages.add_message(request, messages.INFO, msg)
+            return redirect(self.url)
+
+        posts = self.get_posts(tag=tag)
+        context = {
+            'tag': tag,
+            'posts': posts
+        }
+        return render(request, 'blog/blog_index_page.html', context)
+
+    def serve_preview(self, request, mode_name):
+        return self.serve(request)
+
+    def get_posts(self, tag=None):
+        posts = BlogPage.objects.live().descendant_of(self)
+        if tag:
+            posts = posts.filter(tags=tag)
+        return posts
+
+    def get_child_tags(self):
+        tags = []
+        for post in self.get_posts():
+            tags += post.get_tags
+        tags = sorted(set(tags))
+        return tags
