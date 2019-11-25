ee474b3b9e9963867c03ce2a970c4e72315e2dd8
Hyounggyu Kim <code@hyounggyu.com>
2019-11-25 16:45:46 +0900

    Update settings

--- cupweb/settings/base.py
+++ cupweb/settings/base.py
@@ -88,17 +88,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cupweb.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -162,4 +151,4 @@ WAGTAIL_SITE_NAME = "cupweb"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'https://cupweb.ibs.re.kr/'
--- cupweb/settings/dev.py
+++ cupweb/settings/dev.py
@@ -9,6 +9,16 @@ SECRET_KEY = 'gcqa1wmi7ns57y1vllkq91qwvd$ir8uz*)teh%pc#y&z&7fsmt'
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
