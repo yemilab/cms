87c29073426cdddda0458daba4522ac674ff5007
Hyounggyu Kim <code@hyounggyu.com>
2019-11-28 18:09:11 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -ex
 
 until psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db/$POSTGRES_DB -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
@@ -8,7 +8,8 @@ done
 
 >&2 echo "Postgres is up - continuing"
 
-python manage.py migrate --noinput
 python manage.py collectstatic --no-input
+
+python manage.py migrate --no-input
 
 exec gunicorn cupweb.wsgi:application --bind 0.0.0.0:8000 --workers 3
